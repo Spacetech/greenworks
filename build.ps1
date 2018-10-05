61b8aeb759a106ccdc063ac05f39c5b4502990ab
Gary Wilber <Spacetech326@gmail.com>
2018-10-04 22:16:32 -0700

    Update build script

--- build.ps1
+++ build.ps1
@@ -9,9 +9,9 @@ $root = [System.IO.Path]::GetFullPath($PSScriptRoot)
 Remove-Item $root\lib -Force -Recurse -ErrorAction SilentlyContinue
 
 node-gyp rebuild --target=$version --arch=ia32 --dist-url=https://atom.io/download/electron
-git
+
 node-gyp rebuild --target=$version --arch=x64 --dist-url=https://atom.io/download/electron
 
-bash -c "cd /mnt/d/Programming/greenworks && HOME=~/.electron-gyp node-gyp rebuild --target=$version --arch=ia32 --dist-url=https://atom.io/download/electron"
+bash -c "cd /mnt/c/Programming/greenworks && HOME=~/.electron-gyp node-gyp rebuild --target=$version --arch=ia32 --dist-url=https://atom.io/download/electron"
 
-bash -c "cd /mnt/d/Programming/greenworks && HOME=~/.electron-gyp node-gyp rebuild --target=$version --arch=x64 --dist-url=https://atom.io/download/electron"
+bash -c "cd /mnt/c/Programming/greenworks && HOME=~/.electron-gyp node-gyp rebuild --target=$version --arch=x64 --dist-url=https://atom.io/download/electron"
