dd5bdcd7cb51038110f8cb71f25aef5fd6f40d88
Gary Wilber <Spacetech326@gmail.com>
2019-04-23 01:16:19 -0700

    build infos

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+# sudo apt-get install build-essential gcc-multilib g++-multilib
+
 npm install --ignore-scripts
 
 node-gyp rebuild --target=$1 --arch=x64 --dist-url=https://atom.io/download/electron
